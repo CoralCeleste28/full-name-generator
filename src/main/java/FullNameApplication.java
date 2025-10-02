89e632e76ebe853e11fd610921a8580f31305adf
Coral Estrada <coralestrada28@gmail.com>
2025-10-01 20:00:58 -0500

    Added additional notes, cleaned up code, and ran some tests

--- src/main/java/FullNameApplication.java
+++ src/main/java/FullNameApplication.java
@@ -3,56 +3,46 @@ import java.util.Scanner;
 public class FullNameApplication {
     public static void main(String[] args) {
 
+        //Declare variables with no value
         String firstName;
         String middleName;
         String lastName;
         String suffix;
+        String fullName;
         Scanner scanner = new Scanner(System.in);
+        // create scanner that reads the next line
 
         System.out.print("Enter First Name: ");
-        firstName = scanner.nextLine();
+        firstName = scanner.nextLine().trim();
+        // Read the next line
+        // remove all empty spacing before and after a sentence assign it to firstName
 
         System.out.print("Enter Middle Name: ");
-        middleName = scanner.nextLine();
+        middleName = scanner.nextLine().trim(); ;
+        // Read the next line and assign it to middleName
+        // remove all empty spacing before and after a sentence assign it to middleName
 
         System.out.print("Enter Last Name: ");
-        lastName = scanner.nextLine();
+        lastName = scanner.nextLine().trim(); ;
+        // Read the next line and assign it to lastName
+        // remove all empty spacing before and after a sentence assign it to lastName
 
         System.out.print("Enter Suffix: ");
-        suffix = scanner.nextLine();
+        suffix = scanner.nextLine().trim(); ;
+        // Read the next line and assign it to suffix
+        // remove all empty spacing before and after a sentence assign it to
 
-        String fullName = firstName.trim(); // in case they entered "   Coral    "
+        fullName = firstName; // sets firstName to fullName
 
-        if (!middleName.isBlank()) {    //is blank
-            fullName = fullName + " " + middleName.trim() +" ";
+        if (!middleName.isBlank()) { // If middle name is not(!) blank (.isBlank())
+            fullName = fullName + " " + middleName + " "; // full name is fullName middleName
         }
+        fullName = fullName + " " + lastName;
 
-        fullName = fullName + lastName;
-
-        if (!suffix.isBlank()) {
-            fullName = fullName + ", " + suffix.trim();
+        if (!suffix.isBlank()) { // suffix is not (!) blank (.isBlank())
+            fullName = fullName + ", " + suffix; // fullName is fullName , suffix
         }
         System.out.println(fullName);
-
-
-
-
-
-    }
-
-    public FullNameApplication() {
-
-
-    }
-
-    public  void doSomething() {
-
-
-    }
-
-    public static void something() {
-
-
     }
 
 }
